c1a9269fee3d856da2eb3363caa7b91e23c51775
Shivanimyadam <shivanimyadam@gmail.com>
2026-03-07 23:34:43 +0530

    ENH: Added user_id to messages table in DB.Changed code in router function.

--- backend/routes/chat.js
+++ backend/routes/chat.js
@@ -2,6 +2,25 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 const db = require('../db');
+const jwt = require('jsonwebtoken');
+
+// Middleware to verify token
+
+const verifyToken = (req,res,next) => {
+    const token = req.headers.authorization?.split(' ')[1];
+    console.log("token ",req.headers,"---",token);
+    if(!token){
+        return res.status(401).json({error:"No token provided "});
+    }
+    try{
+        const decoded = jwt.verify(token,process.env.JWT_SECRET);
+        console.log("decoded",decoded);
+        req.user = decoded;
+        next(); // moves to next step (route handler)
+    } catch (error) {
+           return res.status(401).json({ error: 'Invalid token' });
+    }
+};
 
 
 // **`routes/chat.js`**
@@ -11,12 +30,13 @@ const db = require('../db');
 // - Gets the AI reply back
 // - Sends the reply to your frontend
 
-router.post('/', async (req, res) => {
+router.post('/',verifyToken, async (req, res) => {
     const { message } = req.body;
+    const userId = req.user.id;
     try {
         // first save user messages to db
         db.query(
-            'INSERT INTO messages (role,text) VALUES (?,?)',['user',message]
+            'INSERT INTO messages (role,text,user_id) VALUES (?,?,?)',['user',message,userId]
         ); 
         const response = await axios.post(
             'https://api.groq.com/openai/v1/chat/completions',
@@ -35,7 +55,7 @@ console.log("Groq response:", JSON.stringify(response.data, null, 2));
         const reply = response.data.choices[0].message.content;
         // save AI reply to db
           db.query(
-            'INSERT INTO messages (role,text) VALUES (?,?)',['ai',reply]
+            'INSERT INTO messages (role,text,user_id) VALUES (?,?,?)',['ai',reply,userId]
         ); 
         res.json({ reply });
 
@@ -49,8 +69,9 @@ console.log("Groq response:", JSON.stringify(response.data, null, 2));
 
 // creating GET route - to load previous messages
 
-router.get('/',(req,res)=>{
-    db.query('SELECT * FROM messages ORDER BY created_at ASC',(err,results)=>{
+router.get('/',verifyToken,(req,res)=>{
+    db.query('SELECT * FROM messages WHERE user_id = ? ORDER BY created_at ASC',
+        [req.user.id],(err,results)=>{
          if (err) {
       console.error(err);
       return res.status(500).json({ error: 'Failed to fetch messages' });
